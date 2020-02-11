3514e6b78bbf6de8b20a14735f2470d840c90127
Tiryoh <tiryoh@gmail.com>
2020-02-11 16:03:16 +0900

    fix: Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
 GITHUB_BASEURL=https://api.github.com
-GITHUB_API="/repos/${INPUT_REPOSITORY:-GITHUB_REPOSITORY}/actions/runs/${INPUT_RUN_ID:-GITHUB_RUN_ID}/jobs"
-curl -H "Authorization: token $GITHUB_TOKEN" -H "Content-Type: application/json" -X GET "$GITHUB_BASEURL$GITHUB_API" \
-| jq ".jobs | map({ name: .name, html_url: .html_url } | select(.name == \"${INPUT_JOB_NAME}\"))" | grep html_url | sed -E 's#.*"html_url": "(.*)".*#\1#g')
+GITHUB_API="/repos/${INPUT_REPOSITORY:-${GITHUB_REPOSITORY}}/actions/runs/${INPUT_RUN_ID:-${GITHUB_RUN_ID}}/jobs"
+curl -H "Authorization: token ${INPUT_GITHUB_TOKEN}" -H "Content-Type: application/json" -X GET "${GITHUB_BASEURL}${GITHUB_API}" \
+| jq ".jobs | map({ name: .name, html_url: .html_url } | select(.name == \"${INPUT_JOB_NAME}\"))" | grep html_url | sed -E 's#.*"html_url": "(.*)".*#\1#g' | tail -n1
 echo ::set-output name=job_id::$(echo ${URL} | sed -E 's#.*runs/(.*)#\1#')
 echo ::set-output name=html_url::${URL}
