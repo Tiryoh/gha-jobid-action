59990a5f284dc74f0c98aa21542375eb41c04060
Daisuke Sato <tiryoh@gmail.com>
2023-09-19 02:09:19 +0900

    feat: Use GitHub Composite Actions

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -l
-
-GITHUB_BASEURL=https://api.github.com
-GITHUB_API="/repos/${INPUT_REPOSITORY:-${GITHUB_REPOSITORY}}/actions/runs/${INPUT_RUN_ID:-${GITHUB_RUN_ID}}/jobs"
-eval "$(curl --get -Ss -H "Authorization: token ${INPUT_GITHUB_TOKEN}" -H "Accept: application/vnd.github.v3+json" "${GITHUB_BASEURL}${GITHUB_API}?per_page=${INPUT_PER_PAGE:-30}" \
-| jq -r --arg job_name "${INPUT_JOB_NAME}" '.jobs | map(select(.name == $job_name)) | .[0] | @sh "job_id=\(.id) html_url=\(.html_url)"')"
-echo "job_id=${job_id}" >> $GITHUB_OUTPUT
-echo "html_url=${html_url}" >> $GITHUB_OUTPUT
